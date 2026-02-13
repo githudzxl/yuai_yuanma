ee552249e6146483a3a728b8fb9b981e140d4704
githudzxl <59473354+githudzxl@users.noreply.github.com>
2026-02-13 20:05:17 +0800

    Add files via upload

---
+++ EyeRecall/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Permissions;
+
